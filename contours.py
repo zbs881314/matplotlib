d9400599b8d4f85d2f198b201b742e97db6f0dd9
bob <shibo.zhou14@gmail.com>
2018-12-31 14:13:12 -0500

    update

--- contours.py
+++ contours.py
@@ -2,7 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x, y):
+    # the hight function
     return (1 - x/2 + x**5 + y**3) * np.exp(-x**2 - y**2)
 
 n = 256
 x = np.linspace(-3, 3, n)
+y = np.linspace(-3, 3, n)
+X, Y = np.meshgrid(x, y)
+
+
+# use plt.contourf to filling contours
+# X, Y and value for (X, Y) points
+
+plt.contourf(X, Y, f(X, Y), 8, alpha=.75, cmap=plt.cm.hot)
+
+# use plt.contour to add contour lines
+C = plt.contour(X, Y, f(X, Y), 8, color='black', linewidth=.5)
+# adding label
+plt.clabel(C, inline=True, fontsize=10)
+plt.xticks(())
+plt.yticks(())
+plt.show()
