2474f6a709f74a2765e935624a4319c6ef3f044c
bob <shibo.zhou14@gmail.com>
2018-12-31 14:43:04 -0500

    update

--- subplot.py
+++ subplot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import matplotlib.gridspec as gridspec
 # example 1:
 ###############################
 plt.figure(figsize=(6, 4))
@@ -37,6 +37,36 @@ plt.plot([0, 1], [0, 3])
 plt.subplot(236)
 # figure splits into 2 rows, 3 col, plot to the 6th sub-fig
 plt.plot([0, 1], [0, 4])
+
+# method 1: subplot2grid
+##########################
+plt.figure()
+ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)  # stands for axes
+ax1.plot([1, 2], [1, 2])
+ax1.set_title('ax1_title')
+ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=2)
+ax3 = plt.subplot2grid((3, 3), (1, 2), rowspan=2)
+ax4 = plt.subplot2grid((3, 3), (2, 0))
+ax4.scatter([1, 2], [2, 2])
+ax4.set_xlabel('ax4_x')
+ax4.set_ylabel('ax4_y')
+ax5 = plt.subplot2grid((3, 3), (2, 1))
+
+# method 2: gridspec
+#########################
+plt.figure()
+gs = gridspec.GridSpec(3, 3)
+# use index from 0
+ax6 = plt.subplot(gs[0, :])
+ax7 = plt.subplot(gs[1, :2])
+ax8 = plt.subplot(gs[1:, 2])
+ax9 = plt.subplot(gs[-1, 0])
+ax10 = plt.subplot(gs[-1, -2])
+
+# method 3: easy to define structure
+####################################
+f, ((ax11, ax12), (ax13, ax14)) = plt.subplots(2, 2, sharex=True, sharey=True)
+ax11.scatter([1,2], [1,2])
 
 
 plt.tight_layout()
